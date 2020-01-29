b0ac69635ab36ba59610aec8b18ebca78f3c3615
Coen Winter <winterc21@asmsa.org>
2020-01-28 23:56:44 -0600

    added tests to all code written so far, and added code for team on base percentage.

--- Capstone initial code.py
+++ Capstone initial code.py
@@ -19,7 +19,7 @@ def indiv_obpa(pitcher,date):
 	for row  in data:
 		x={header:value for header,value in zip(df.columns,row)}
 		if pitcher==x["STARTING PITCHER"] and date>=x["DATE"]:
-			print(x)
+			# ~ print(x)
 			games.add(x["GAME-ID"])
 	for row  in data:
 		x={header:value for header,value in zip(df.columns,row)}
@@ -30,7 +30,7 @@ def indiv_obpa(pitcher,date):
 			ph+=x["PH"]
 			pbb+=x["PBB"]
 			hb+=x["HB"]
-	print(op_ab,ph,pbb,hb,total)
+	# ~ print(op_ab,ph,pbb,hb,total)
 	return (ph+pbb+hb)/op_ab
 
 
@@ -58,16 +58,45 @@ def team_obpa(team,date):
 			ph+=x["PH"]
 			pbb+=x["PBB"]
 			hb+=x["HB"]
-	print(op_ab,ph,pbb,hb,total)
-	return (ph+pbb+hb)/op_ab		
-			
-def indiv_count (pitcher,date,value,op)
+	# ~ print(op_ab,ph,pbb,hb,total)
+	return (ph+pbb+hb)/op_ab
+	
 
-def team_count (team,date,value,op)
+
+def team_obp(team,date):
+	ab=0
+	bh=0
+	bbb=0
+	op_hb=0
+	total=0
+	if type(date) != pd.Timestamp:
+		date=pd.Timestamp(date)
+	games=set()
+	for row  in data:
+		x={header:value for header,value in zip(df.columns,row)}
+		if team==x["TEAM"] and date>=x["DATE"]:
+			# ~ print(x)
+			games.add(x["GAME-ID"])
+	for row  in data:
+		x={header:value for header,value in zip(df.columns,row)}
+		if x["GAME-ID"] in games and team != x["TEAM"]:
+			total+=1
+			op_hb+=x["HB"]
+		if x["GAME-ID"] in games and team == x["TEAM"]:
+			ab+=x["AB"]
+			bh+=x["PH"]
+			bbb+=x["PBB"]
+	# ~ print(ab,bh,bbb,op_hb,total)
+	return (bh+bbb+op_hb)/ab		
+			
+# ~ def indiv_count (pitcher,date,value,op)
+
+# ~ def team_count (team,date,value,op)
 	
 	
-# ~ print(indiv_obpa("Charlie Morton","7/18/19"))
-print(team_obpa("Tampa Bay Rays","7/18/19"))
+print(indiv_obpa("Charlie Morton","10/1/19"))
+print(team_obpa("Tampa Bay Rays","10/1/19"))
+print(team_obp("Tampa Bay Rays","10/1/19"))
 
 # ~ print(df[["BBB","BH"]])
 # ~ preint(np.array(df[["BR","BH","BBB","AVG","W","PH","PR"]]))
